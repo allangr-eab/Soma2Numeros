2aeed4aa44c048d622420043ab8b3322c2b5d567
Allangr-eab <allangabrielrvsantos@yahoo.com.br>
2020-12-13 20:29:26 -0300

    Adiciona a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+            int valor1;
+            int valor2;
+
+            Console.WriteLine("Digite o primeiro valor:");
+            valor1 = int. Parse(Console. ReadLine());
+
+            Console. WriteLine("Digite o 2ºValor: " );
+            valor2 = int. Parse(Console. ReadLine());
+            Console. WriteLine(\n" );
+
+            
+            Console. WriteLine($"Seu resultado é: {valor1 + valor2}" );
+
+            Console. WriteLine("Pressione ENTER para sair" );
+            Console. ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Soma2Numeros
+
+**Enunciado**:
+
+Leia 2 números inteiros e exiba o resultado da soma:
+
+Cálculo da soma entre dois números.
+
+Digite o primeiro número: 30 Dígitos o segundo número: 40
+
+Soma: 70
+
+**Como Executar**
+
+Acesse a pasta do projeto
+
+´´´
+dotnet run
+```
